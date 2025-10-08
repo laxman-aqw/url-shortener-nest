ed58b1439a3684cffe9e64189473c12fbee25aa0
Laxman Rumba <rumbavai767@gmail.com>
2025-10-08 10:13:16 +0545

    feat(auth): implement auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { SignUpUserDto } from './dto/signup-user-dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+  async signUp(signUpUserDto: SignUpUserDto): Promise<any> {
+    const user = await this.userService.create(signUpUserDto);
+    if (user?.password !== 'pass') {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+    //todo jwt
+    return result;
+  }
+}
